93062383e1feab10ce66c8d24246a0badcdceb6f
Bak8 <ataibegaliev@gmail.com>
2022-11-24 16:27:04 +0600

    ver 1

--- HomeWork 3_2/ViewController.swift
+++ HomeWork 3_2/ViewController.swift
@@ -8,10 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var birthTextField: UITextField!
+    
+    @IBOutlet weak var maleOutput: UILabel!
+    @IBAction func maleSwitch(_ sender: UISwitch) {
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondScreen") as! SecondViewController
+        var genderOutput = ""
+        if sender.isOn {
+            genderOutput = "Male"
+        }else{
+            genderOutput = "Female"
+        }
+        secondVC.labelGender2 = genderOutput
+    }
+   
+    
+    
+    @IBAction func buttonFor2screen(_ sender: Any) {
+        var textUserName = userNameTextField.text
+        var textEmail = emailTextField.text
+        var textPhone = phoneTextField.text
+        var textBirth = birthTextField.text
+        var textGender = maleOutput.text
+        
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondScreen") as! SecondViewController
+        navigationController?.pushViewController(secondVC, animated: true)
+        
+        secondVC.textUserName2 = textUserName ?? ""
+        secondVC.textEmail2 = textEmail ?? ""
+        secondVC.textPhone2 = textPhone ?? ""
+        secondVC.textBirth2 = textBirth ?? ""
+        secondVC.labelGender2 = textGender ?? ""
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Profile"
     }
 
 
